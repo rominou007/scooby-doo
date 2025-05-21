05cc50d2e2fd565ee0e996c00862c52e2cfa1d43
Nazim ILLOULI <illoulinazim06@gmail.com>
2025-05-21 20:45:28 +0200

    jsp

--- edit_user.php
+++ edit_user.php
@@ -4,18 +4,18 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     require("db.php");
-    $id = $_POST["id"];
-    $username = $_POST["username"];
-    $name = $_POST["name"];
-    $access = $_POST["access"];
+    $user_id = $_POST["id_user"];
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $role = $_POST["role"];
 
     if(empty($username) || empty($name)){
         die("Veuillez remplir tous les champs !");
     }
 
-    $sql = "UPDATE users SET username = :username, name = :name, access = :access WHERE id = :id";
+    $sql = "UPDATE user SET nom = :nom, prenom = :prenom,  role = :role WHERE id_user = :id_user";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute(['username' => $username, 'name' => $name, 'access' => $access, 'id' => $id]);
+    $stmt->execute([, 'nom' => $nom, 'prenom' => $prenom, 'role' => $role, 'user_id' => $id_user]);
     
     header("location: users.php");
 } else {
